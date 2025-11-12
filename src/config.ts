c06d7a343121182a1829e9c902883f7b6c2279a3
AT13xe <845043294@qq.com>
2025-11-12 23:20:53 +0800

    Add internal link for friends section

--- src/config.ts
+++ src/config.ts
@@ -63,11 +63,11 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.Home,
 		LinkPreset.Archive,
 		LinkPreset.About,
-		// {
-			// name: "友链",
-			// url: "/friends/", // Internal links should not include the base path, as it is automatically added
-			// external: false, // Show an external link icon and will open in a new tab
-		// },
+		{
+			name: "友链",
+			url: "/friends/", // Internal links should not include the base path, as it is automatically added
+			external: false, // Show an external link icon and will open in a new tab
+		},
 		// {
 			// name: "赞助",
 			// url: "/sponsors/", // Internal links should not include the base path, as it is automatically added
